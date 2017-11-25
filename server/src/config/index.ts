e439f0aabe21f292ea190fbe6caab1e3017c517d
Larry Faudree <lfaudreejr@hotmail.com>
2017-11-24 20:43:41 -0500

    setting up server files

---
+++ server/src/config/index.ts
@@ -0,0 +1,5 @@
+import * as dotenv from 'dotenv';
+
+export default {
+  PORT: process.env.PORT
+};
---
+++ server/src/index.ts
@@ -0,0 +1,74 @@
+import * as express from 'express';
+import * as compression from 'compression';
+import * as http from 'http';
+import * as path from 'path';
+import config from './config';
+
+const favicon = require('serve-favicon');
+/**
+ * Express setup
+ */
+const app = express();
+const server = new http.Server(app);
+
+const port = normalizePort(config.PORT || 3000);
+app.set('port', port);
+/*
+	Middleware
+*/
+app.use(compression());
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+/**
+ * Normalize a port into a number, string, or false.
+ */
+function normalizePort (val: any) {
+  const port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+/**
+ * Event listener for HTTP server "error" event.
+ */
+function onError (error: any) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  let bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+function onListening () {
+  const addr = server.address();
+  let bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
+  console.log('Listening on ' + bind);
+}
