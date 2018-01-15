74821c44fc52871273bd22620d8734807a7f217d
Larry Faudree <lfaudreejr@hotmail.com>
2018-01-14 21:56:57 -0500

    remove dotenv for nodeforeman

--- server/src/api/routes.ts
+++ server/src/api/routes.ts
@@ -5,10 +5,7 @@ import tradeRoutes from './trades/trade-routes'
 import googleRoutes from './googleApis/googleBooks-routes'
 
 const router = express.Router()
-router.use(function(req, res, next) {
-  console.log('%s %s %s', req.method, req.url, req.path);
-  next();
-});
+
 router.use('/books', bookRoutes)
 router.use('/user', userRoutes)
 router.use('/trades', tradeRoutes)
--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -1,4 +1,3 @@
-require('dotenv').config();
 
 export const config = {
   PORT: process.env.PORT,
